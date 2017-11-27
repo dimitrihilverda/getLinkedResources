a4e8e6a309d8ae7e44c2e9a6d22b881f49340cfb
Dimitri Hilverda <spekbroodje@gmail.com>
2017-11-27 09:56:53 +0100

    Update getLinkedResources.snippet.php
    Made it a lot simpler

--- getLinkedResources.snippet.php
+++ getLinkedResources.snippet.php
@@ -1,13 +1,42 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 $docId = $modx->getOption('docId', $scriptProperties, $modx->resource->get('id'));
 $tpl = $modx->getOption('tpl', $scriptProperties, '');
 
-if (is_numeric($docId) && !empty($docId)) {
-	$page = $modx->getObject('modResource', $docId);
+if (!is_numeric($docId) && empty($docId)) {
+	return false;
+}
 
-//see if the id given is used in content if so the this is a original id and I get all symlinks then I have an array with symlinks and I have the original id
-	$symlinkObjs = $modx->getCollection('modResource', array('content' => $docId));
-	if (count($symlinkObjs) != 0) {
+$currentResource = $modx->getObject('modResource', $docId);
+
+if ($currentResource->get('class_key') == 'modSymLink') {
+	//is symlink
+	$docId = $currentResource->get('content');
+	if (!is_numeric($docId) && empty($docId)) {
+		return 'foutje';
+	}
+}
+
+$modDocument = $modx->getObject('modResource', $docId);
+$symlinkObjs = $modx->getCollection('modResource', array('content' => $docId));
+
+if (count($symlinkObjs) != 0) {
+	if ($tpl != '' ) {
+		$output .= $modx->getChunk(
+			$tpl,
+			array(
+				'id' => $modDocument->get('id'),
+				'parent' => $modDocument->get('parent'),
+				'pagetitle' => $modDocument->get('pagetitle'),
+				'alias' => $modDocument->get('alias')
+			)
+		);
+	}
+
+	// loop tru each symlink
+	foreach($symlinkObjs as $page) {
 		if ($tpl != '') {
 			$output .= $modx->getChunk(
 				$tpl,
@@ -19,66 +48,16 @@ if (is_numeric($docId) && !empty($docId)) {
 				)
 			);
 		}
-		foreach($symlinkObjs as $page) {
-			//print_r($symlinkObj, true);
-			if (is_numeric($page->get('content')) && !empty($page->get('content'))) {
-				if ($tpl != '') {
-					$output .= $modx->getChunk(
-						$tpl,
-						array(
-							'id' => $page->get('id'),
-							'parent' => $page->get('parent'),
-							'pagetitle' => $page->get('pagetitle'),
-							'alias' => $page->get('alias')
-						)
-					);
-				}
-				else {
-					$output[] = intval($page->get('id'));
-				}
-			}
-		}
-	} else {
-	//its a symlink lets see if there are more symlinks
-	//if the id is not found as content then this could be 2 reasons, 1 this id is not used at all or this is a symlink, if this is a symlink then try to find this id as resource and check if the content is an ID then us that Id to do the same as above
-
-	$output .= $modx->getChunk(
-		$tpl,
-		array(
-			'id' => $page->get('id'),
-			'parent' => $page->get('parent'),
-			'pagetitle' => $page->get('pagetitle'),
-			'alias' => $page->get('alias')
-		)
-	);
-
-	$symlinkObjs = $modx->getIterator('modResource', array('content' => $page->get('content')));
-
-	foreach($symlinkObjs as $page) {
-
-		if (is_numeric($page->get('content')) && !empty($page->get('content'))) {
-			if ($tpl != '') {
-				$output .= $modx->getChunk(
-					$tpl,
-					array(
-						'id' => $page->get('id'),
-						'pagetitle' => $page->get('pagetitle'),
-						'alias' => $page->get('alias'),
-						'parent' => $page->get('parent')
-					)
-				);
-			}
-			else {
-				$output[] = $page->get('id');
-			}
+		else {
+			$output[] = intval($page->get('id'));
 		}
 	}
 }
 
-}
-
 if (is_array($output)) {
-	$output[] = $docId;
+	if ($showSelf != 0) {
+		$output[] = $docId;
+	}
 	$output = implode(',', $output);
 }
 
